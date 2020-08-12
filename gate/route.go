4e0450082a7277d2d229b2db7512878b60aaeda6
Luqmanul Hakim <n.loekman@gmail.com>
2020-08-12 10:16:07 +0700

    adjustment endpoint route

--- gate/route.go
+++ gate/route.go
@@ -1,22 +1,24 @@
 package gate
+
 import (
-    "github.com/gin-gonic/gin"
-    "wa-service/action/auth"
-    "wa-service/action/message"
-    "wa-service/app"
+	"wa-service/action/auth"
+	"wa-service/action/message"
+	"wa-service/app"
+
+	"github.com/gin-gonic/gin"
 )
 
 func Route() {
-    r := gin.New()
-    r.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "pong",
-        })
-    })
-    r.Use(Middleware())
-    r.Static("/storage/file", app.PATH_FILE)
-    r.GET("/login", auth.Login())
-    r.POST("/send/message", message.SendMessage())
-    r.Use(gin.Recovery())
-    _ = r.Run()
+	r := gin.New()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Static("/storage/file", app.PATH_FILE)
+	r.Use(Middleware())
+	r.GET("/login", auth.Login())
+	r.POST("/send/message", message.SendMessage())
+	r.Use(gin.Recovery())
+	_ = r.Run()
 }
